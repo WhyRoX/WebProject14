dac5a4f77d00bf86c2bdbf8bc120acf7c0ea4b9a
WhyRoX <117106090+WhyRoX@users.noreply.github.com>
2024-03-19 20:45:25 +0100

    Update coaches.js

--- routes/coaches.js
+++ routes/coaches.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-/* Get coaches page. */
+/* GET coaches page. */
 router.get('/', (req, res) => {
     res.render('coach.hbs');
 });
