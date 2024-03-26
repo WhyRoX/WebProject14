6656727bef755968fe37e9ab73e66ac391682cb6
Enes Yumer <enes.12.yumer@gmail.com>
2024-03-26 09:49:08 +0100

    updateFiles

--- routes/members.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET members page. */
-router.get('/', (req, res) => {
-    res.render('members/index.hbs');
-});
-
-module.exports = router;
