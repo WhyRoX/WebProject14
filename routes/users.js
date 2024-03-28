e5824fae3ca6a0a986a18eb84bd2283f0e4a6f7b
WhyRoX <enes.12.yumer@gmail.com>
2024-03-29 00:26:28 +0100

    Navbar + register page + css

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
-/* GET tournaments page. */
+/* GET user page. */
 router.get('/', (req, res) => {
-    res.render('users/index.hbs');
+    res.render('users/index.hbs'); // Render the user page (which will be used for both login and registration)
+});
+
+/* GET user login page. */
+router.get('/login', (req, res) => {
+    res.render('users/index.hbs'); // Render the login page using the same "index.hbs" file
+});
+
+/* GET user register page. */
+router.get('/register', (req, res) => {
+    res.render('users/register.hbs'); // Render the register page
 });
 
 module.exports = router;
