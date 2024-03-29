c99acd12993810238b4f717ec02b1a214e1d1e9f
WhyRoX <enes.12.yumer@gmail.com>
2024-03-30 00:22:28 +0100

    Layout + Users + Login/register

--- models/User.js
+++ models/User.js
@@ -1,1 +1,20 @@
 const db = require("./db_conf.js");
+
+/* Creating a new user */
+module.exports.save = (firstname, surname, email, password, status) => {
+    const stmt = db.prepare("INSERT INTO users (firstname, surname, email, password, status) VALUES (?, ?, ?, ?, ?)");
+    const info = stmt.run(firstname, surname, email, password, status);
+};
+
+/* Getting encrypted password by email to login */
+module.exports.login = (email) => {
+    const stmt = db.prepare("SELECT password FROM users WHERE email = ?");
+    return stmt.get(email).password;
+};
+
+/* Getting a user info by email */
+module.exports.data = (email) => {
+    const stmt = db.prepare("SELECT * FROM users WHERE email = ?");
+    return stmt.get(email);
+};
+
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User.js');
 const bcrypt = require('bcrypt');
+
 
 /* GET user page. */
 router.get('/', (req, res) => {
@@ -9,12 +11,44 @@ router.get('/', (req, res) => {
 
 /* GET user login page. */
 router.get('/login', (req, res) => {
-    res.render('users/index.hbs'); // Render the login page using the same "index.hbs" file
+    res.render('users/index.hbs', { errors: req.session.errors });
+    req.session.errors = null;// Render the login page using the same "index.hbs" file
+});
+
+/* POST user login page. */
+router.post("/login/connect", (req, res) => {
+    if (bcrypt.compareSync(req.body.password, User.login(req.body.email))) {
+        req.session.connected = true;
+        req.session.user = User.data(req.body.email);
+        res.redirect("/");
+    } else {
+        req.session.errors = "Mot de passe incorrect";
+        res.redirect("/users/login");
+    }
 });
 
 /* GET user register page. */
 router.get('/register', (req, res) => {
     res.render('users/register.hbs'); // Render the register page
+});
+
+/* POST user register page. */
+router.post('/add', (req, res) => {
+    const User = require('../models/User.js');
+    const firstname = req.body.firstname;
+    const surname = req.body.surname;
+    const email = req.body.email;
+    const password = bcrypt.hashSync(req.body.password, 10);
+    const status = 'regular';
+    User.save(firstname, surname, email, password, status);
+    req.session.connected = true;
+    res.redirect('/'); // Redirect to the login page
+});
+
+/* GET user logout page. */
+router.get('/logout', (req, res) => {
+    req.session.destroy(); // Destroy the session
+    res.redirect('/'); // Redirect to the home page
 });
 
 module.exports = router;
