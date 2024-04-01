bac266d48776d94ed2c50d598daded6b268cecc0
AshuuIPL <Ashutosh.manshrestha@student.vinci.be>
2024-04-01 23:25:36 +0200

    qsqsqqs

--- routes/coaches.js
+++ routes/coaches.js
@@ -3,19 +3,15 @@ const router = express.Router();
 
 /* GET coaches page. */
 router.get('/', (req, res) => {
-    res.render('coaches/index.hbs');
+    res.render('coaches/index.hbs', { session: req.session });
 });
 
 router.get('/bio', (req, res) => {
-    res.render('coaches/details.hbs');
+    res.render('coaches/details.hbs', { session: req.session });
 });
 
 router.get('/bio/send', (req, res) => {
-    res.render('coaches/index.hbs');
+    res.render('coaches/index.hbs', { session: req.session });
 });
-
-
-
-
 
 module.exports = router;
