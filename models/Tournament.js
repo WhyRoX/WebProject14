21fd18d3df3dfae9c5796e5867eae6b1f9698361
Zak144hz <zakaria.elberaichi@student.vinci.be>
2024-04-02 10:53:08 +0200

    .

--- models/Tournament.js
+++ models/Tournament.js
@@ -1,1 +1,35 @@
-const db = require("./db_conf.js");
+const db = require('../models/db_conf');
+
+
+module.exports.list = () => {
+    return db.prepare("SELECT * FROM tournaments").all();
+};
+
+
+module.exports.findById = (id) => {
+    return db.prepare("SELECT * FROM tournaments WHERE tournaments_id = ?").get(id);
+};
+
+
+module.exports.save = (data) => {
+    console.log("SAVE :" + JSON.stringify(data));
+    //no id => add exoplanet
+    if (data.id === undefined) {
+        const stmt = db.prepare('INSERT INTO TOURNAMENTS(unique_name, hclass, discovery_year, image) VALUES (?, ?, ?, ?)');
+        const info = stmt.run(data.uniqueName, data.hClass, data.discoveryYear, data.image);
+        console.log("tournament model save" + info.changes);
+    }
+    // id => update exoplanet
+    else {
+        const stmt = db.prepare('UPDATE TOURNAMENT SET unique_name = ?, hclass = ?, discovery_year = ?, ist = ?, pclass = ? WHERE exoplanet_id = ?');
+        const info = stmt.run(data.uniqueName, data.hClass, data.discoveryYear, data.IST, data.pClass, data.id);
+        console.log("exoplanet model save update" + info.changes);
+
+    }
+
+
+
+
+};
+
+    
--- routes/tournaments.js
+++ routes/tournaments.js
@@ -1,9 +1,53 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET tournaments page. */
-router.get('/', (req, res) => {
-    res.render('tournaments/index.hbs');
+
+
+/* GET tournaments index. */
+router.get('/', function (req, res, next) {
+    const tournamentsTable = Tournament.list();
+    res.render('exoplanets/index.hbs', { tournamentsTable });
 });
+
+
+/* GET details exoplanet. */
+router.get('/details', function (req, res, next) {
+    console.log("GET DETAILS TOURNAMENTS");
+    // convert string req.query.id to int
+    // another solution is to use == instead of === in if instruction
+    const tournamentsIdParam = parseInt(req.query.id);
+    const tournamentsFound = Tournament.findById(tournamentsIdParam);
+    res.render('exoplanets/details.hbs', { tournament: tournamentsFound  });
+
+});
+
+
+
+
+/* POST add exoplanet. */
+router.post('/create', upload.single('imageTournament'), function (req, res, next) {
+    console.log("POST ADD TOURNAMENT");
+    // validate name of explanet -> betweeen 3 and 100 character
+    if (validator.isLength(req.body.uniqueNameTournament, { min: 3, max: 100 })) {
+        console.log("req.file : " + JSON.stringify(req.file));
+        let filename = null;
+        // req.file must be undefined if no file given
+        if (req.file === undefined) filename = null;
+        else filename = 'images/' + req.file.filename;
+        Tournament.save({
+            uniqueName: req.body.uniqueNameExoplanet,
+            hClass: req.body.hClassExoplanet,
+            discoveryYear: req.body.discoveryYearExoplanet,
+            image: filename
+        });
+        res.redirect('/exoplanets');
+    }
+    else {
+        res.redirect('/exoplanets?errors= Le nom d\'une exoplanète doit faire entre 3 et 100 caractères');
+    }
+});
+
+
+
 
 module.exports = router;
