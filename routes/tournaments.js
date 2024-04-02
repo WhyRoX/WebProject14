cb8cbe4914c3172322d2d69f98f4f2eedffa724a
Zak144hz <zakaria.elberaichi@student.vinci.be>
2024-04-02 12:44:34 +0200

    .

--- routes/tournaments.js
+++ routes/tournaments.js
@@ -1,52 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const Tournament = require('../models/Tournament.js');
+
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, 'public/images/courts/');
+  },
+  filename: function (req, file, cb) {
+      const date = new Date();
+    const uniquePrefix = date.getFullYear() + '-' +
+      (date.getMonth() + 1) + '-' + date.getDate() + '-' +
+      date.getHours() + '-' + date.getMinutes() + '-' + date.getSeconds();
+      cb(null, uniquePrefix + '-' + file.originalname);
+  }
+})
+
+const upload = multer({ storage: storage });
 
 
 
 /* GET tournaments index. */
 router.get('/', function (req, res, next) {
     const tournamentsTable = Tournament.list();
-    res.render('exoplanets/index.hbs', { tournamentsTable });
+    res.render('tournaments/index.hbs', { tournamentsTable });
 });
 
 
-/* GET details exoplanet. */
+/* GET details tournaments */
 router.get('/details', function (req, res, next) {
     console.log("GET DETAILS TOURNAMENTS");
     // convert string req.query.id to int
     // another solution is to use == instead of === in if instruction
     const tournamentsIdParam = parseInt(req.query.id);
     const tournamentsFound = Tournament.findById(tournamentsIdParam);
-    res.render('exoplanets/details.hbs', { tournament: tournamentsFound  });
+    res.render('tournaments/details.hbs', { tournament: tournamentsFound  });
 
 });
-
-
-
-
-/* POST add exoplanet. */
-router.post('/create', upload.single('imageTournament'), function (req, res, next) {
-    console.log("POST ADD TOURNAMENT");
-    // validate name of explanet -> betweeen 3 and 100 character
-    if (validator.isLength(req.body.uniqueNameTournament, { min: 3, max: 100 })) {
-        console.log("req.file : " + JSON.stringify(req.file));
-        let filename = null;
-        // req.file must be undefined if no file given
-        if (req.file === undefined) filename = null;
-        else filename = 'images/' + req.file.filename;
-        Tournament.save({
-            uniqueName: req.body.uniqueNameExoplanet,
-            hClass: req.body.hClassExoplanet,
-            discoveryYear: req.body.discoveryYearExoplanet,
-            image: filename
-        });
-        res.redirect('/exoplanets');
-    }
-    else {
-        res.redirect('/exoplanets?errors= Le nom d\'une exoplanète doit faire entre 3 et 100 caractères');
-    }
-});
-
 
 
 
