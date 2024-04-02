1e91dc874bda8480aa7aa6839b33c6495e2e31be
AshuuIPL <Ashutosh.manshrestha@student.vinci.be>
2024-04-02 12:44:12 +0200

    last push

--- models/Coach.js
+++ models/Coach.js
@@ -1,1 +1,11 @@
 const db = require("./db_conf.js");
+
+module.exports.info = () => {
+    const stmt = db.prepare("SELECT * FROM users");
+    return stmt.all();
+}
+
+module.exports.findById = (user_id) => {
+    const stmt = db.prepare("SELECT * FROM users WHERE user_id = ?");
+    return stmt.get(user_id);
+};
--- routes/coaches.js
+++ routes/coaches.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const Coach = require('../models/Coach.js');
 
 /* GET coaches page. */
 router.get('/', (req, res) => {
-    res.render('coaches/index.hbs', { session: req.session });
+  const errors = req.session.errors;
+  req.session.errors = null;
+    res.render('coaches/index.hbs', { session: req.session,  coaches: Coach.info(), errors: errors });
 });
+
+router.get('/details', (req, res) => {
+    const userId = req.query.user_id;
+    
+    // Check if courtId is a number
+    if (isNaN(userId)) {
+      req.session.errors = 'Invalid user id';
+      res.redirect('/coaches');
+      return;
+    }
+  
+    const coach = Coach.findById(userId);
+  
+    // Check if court exists
+    if (!coach) {
+      req.session.errors = 'Court not found';
+      res.redirect('/coaches');
+      return;
+    }
+  
+    res.render('coaches/details.hbs', { coach });
+  });
+
 
 router.get('/bio', (req, res) => {
     res.render('coaches/details.hbs', { session: req.session });
@@ -13,5 +39,6 @@ router.get('/bio', (req, res) => {
 router.get('/bio/send', (req, res) => {
     res.render('coaches/index.hbs', { session: req.session });
 });
+
 
 module.exports = router;
